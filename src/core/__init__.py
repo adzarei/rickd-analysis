be013c6c9d6ccedae21d37e53ed57bd88ea2f42c
Adrian Zapater <X257375@one.merckgroup.com>
2025-04-13 23:00:17 +0200

    Migrate poetry to package mode and create core package

---
+++ src/core/__init__.py
@@ -0,0 +1,6 @@
+"""Core package for RICKD analysis project."""
+
+from . import constants
+from . import utils
+
+__all__ = ['constants', 'utils']
---
+++ src/core/constants.py
@@ -0,0 +1,3 @@
+"""Constants module for the RICKD analysis project."""
+
+RICKD_ROOT_DATA_FOLDER = "/Users/adrianzapaterreig/Documents/Personal/TFM/data/Running Injury Clinic Kinematic Dataset" 
