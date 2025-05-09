bbc2cd816bbc577fd0a674dcdace0dfec1478428
Adrian Zapater <X257375@one.merckgroup.com>
2025-05-09 19:49:22 +0200

    WIP general EDA

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,3 +1,6 @@
 """Constants module for the RICKD analysis project."""
 
 RICKD_ROOT_DATA_FOLDER = "/Users/adrianzapaterreig/Documents/Personal/TFM/data/Running Injury Clinic Kinematic Dataset" 
+RICKD_SOURCE_DATA_FOLDER = f"{RICKD_ROOT_DATA_FOLDER}/source_data"
+RICKD_PROCESSED_DATA_FOLDER = f"{RICKD_ROOT_DATA_FOLDER}/processed_data"
+RICKD_RESULTS_FOLDER = f"{RICKD_ROOT_DATA_FOLDER}/results"
--- src/core/utils.py
+++ src/core/utils.py
@@ -1,1 +1,24 @@
-"""Utility functions for the RICKD analysis project.""" 
+"""Utility functions for the RICKD analysis project."""
+
+import pandas as pd
+
+
+def non_na_count(df, cols: list[str]) -> int:
+    """Count the number of non-NA values in the columns of a dataframe."""
+    return df[cols].notna().any(axis=1).sum()
+
+
+def non_na_percentage(df, cols: list[str]) -> float:
+    """Percentage of non-NA values in the columns of a dataframe."""
+    return non_na_count(df, cols) / len(df) * 100
+
+
+def condition_report(df, df_condition: pd.Series) -> None:
+    """Report the number of rows that meet a condition."""
+    filtered_df = df[df_condition]
+
+    print(f"Number of subjects that meet condition: {len(filtered_df)}")
+    print(f"Percentage of total rows: {(len(filtered_df) / len(df)) * 100:.2f}%")
+
+    print("\nExample rows:")
+    print(filtered_df.head().to_string())
